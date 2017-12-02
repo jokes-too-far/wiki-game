88556ecfb65d8c00a5b2665a8aa7b5949d3310b0
Evan <evan@projectpixelpress.com>
2017-12-02 11:38:59 -0600

    returning giant list of pages, filtered down to remove meta pages

--- src/states/menu.js
+++ src/states/menu.js
@@ -2,7 +2,9 @@ import Request from 'request'
 
 // const url = 'https://simple.wikipedia.org/wiki/Baghlan_Province'
 // const url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5'
-const url = 'https://simple.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&format=json&origin=*'
+const url = 'https://simple.wikipedia.org/w/api.php?action=query&list=random&rnlimit=500&format=json&origin=*'
+
+const badTitles = ['Talk', 'User', 'Template', 'Category', 'Wikipedia:', 'Mediawiki', 'Module']
 
 
 class Menu extends Phaser.State {
@@ -17,9 +19,20 @@ class Menu extends Phaser.State {
       font: '42px Arial', fill: '#ffffff', align: 'center'
     });
     text.anchor.set(0.5);
+	let pages;
 
 	Request(url, (err, response, body) => {
-		console.log(JSON.parse(body).query.random[0].title);
+		const parsed = JSON.parse(body);
+		const potentials = parsed.query.random;
+		pages = potentials.filter(page => {
+			for (const badTitle of badTitles) {
+				if (page.title.toUpperCase().indexOf(badTitle.toUpperCase()) !== -1) {
+					return false;
+				}
+			}
+			return true;
+		})
+		console.log(pages);
 	})
 
     this.input.onDown.add(this.startGame, this);
