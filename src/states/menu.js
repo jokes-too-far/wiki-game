e38f122a79d824eb0e0ddd8e2a4ca88b1d3267e3
Evan <evan@projectpixelpress.com>
2017-12-02 11:27:33 -0600

    get title for a truly random Wikipedia page

--- src/states/menu.js
+++ src/states/menu.js
@@ -1,3 +1,10 @@
+import Request from 'request'
+
+// const url = 'https://simple.wikipedia.org/wiki/Baghlan_Province'
+// const url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5'
+const url = 'https://simple.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&format=json&origin=*'
+
+
 class Menu extends Phaser.State {
 
   constructor() {
@@ -5,10 +12,15 @@ class Menu extends Phaser.State {
   }
 
   create() {
+
     const text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'MENU', {
       font: '42px Arial', fill: '#ffffff', align: 'center'
     });
     text.anchor.set(0.5);
+
+	Request(url, (err, response, body) => {
+		console.log(JSON.parse(body).query.random[0].title);
+	})
 
     this.input.onDown.add(this.startGame, this);
   }
